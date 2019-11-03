0de006e6a79b02f94d49dbfc7a0b01f6abbc8ff7
Tyla Devon <tyladevon@gmail.com>
2019-11-03 14:30:22 -0700

    Refactor User Spec to include new function for adding addresses.

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'User Registration' do
     it 'I see a link to register as a user' do
       visit root_path
 
-      click_link 'Register'
+      click_on 'Register'
 
       expect(current_path).to eq(registration_path)
     end
@@ -14,17 +14,14 @@ RSpec.describe 'User Registration' do
       visit registration_path
 
       fill_in 'Name', with: 'Megan'
-      fill_in 'Address', with: '123 Main St'
-      fill_in 'City', with: 'Denver'
-      fill_in 'State', with: 'CO'
-      fill_in 'Zip', with: '80218'
       fill_in 'Email', with: 'megan@example.com'
       fill_in 'Password', with: 'securepassword'
       fill_in 'Password confirmation', with: 'securepassword'
-      click_button 'Register'
+      click_on 'Submit'
 
-      expect(current_path).to eq(profile_path)
-      expect(page).to have_content('Welcome, Megan!')
+      user = User.last
+
+      expect(current_path).to eq("/users/#{user.id}/addresses/new")
     end
 
     describe 'I can not register as a user if' do
@@ -32,33 +29,23 @@ RSpec.describe 'User Registration' do
         visit registration_path
 
         fill_in 'Name', with: 'Megan'
-        click_button 'Register'
-
-        expect(page).to have_button('Register')
-        expect(page).to have_content("address: [\"can't be blank\"]")
-        expect(page).to have_content("city: [\"can't be blank\"]")
-        expect(page).to have_content("state: [\"can't be blank\"]")
-        expect(page).to have_content("zip: [\"can't be blank\"]")
+        click_on 'Submit'
+        expect(current_path).to eq(users_path)
         expect(page).to have_content("email: [\"can't be blank\"]")
         expect(page).to have_content("password: [\"can't be blank\"]")
       end
 
       it 'I use a non-unique email' do
-        user = User.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'securepassword')
+        user = User.create(name: 'Megan', email: 'megan@example.com', password: 'securepassword')
 
         visit registration_path
 
         fill_in 'Name', with: user.name
-        fill_in 'Address', with: user.address
-        fill_in 'City', with: user.city
-        fill_in 'State', with: user.state
-        fill_in 'Zip', with: user.zip
-        fill_in 'Email', with: user.email
+        fill_in 'Email', with: 'megan@example.com'
         fill_in 'Password', with: user.password
         fill_in 'Password confirmation', with: user.password
-        click_button 'Register'
+        click_on 'Submit'
 
-        expect(page).to have_button('Register')
         expect(page).to have_content("email: [\"has already been taken\"]")
       end
     end
