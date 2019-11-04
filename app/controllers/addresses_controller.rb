dfac2bb266bd2a3053c47212f0e5ab36ee77f5fc
Tyla Devon <tyladevon@gmail.com>
2019-11-04 13:16:37 -0700

    Add create action in AddressController w/private params.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -4,4 +4,22 @@ class AddressesController < ApplicationController
     @address = Address.new
     @user = User.find(params[:user_id])
   end
+
+  def create
+    @user = current_user
+    @address = @user.addresses.create(address_params)
+    if @address.save
+      redirect_to profile_path
+      flash[:success] = "Welcome #{@user.name}!"
+    else
+      flash[:error] = "Please fill in all fields to finish registration"
+      redirect_to "/users/#{@user.id}/addresses/new"
+    end
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:nickname, :name, :address, :city, :state, :zip)
+  end
+
 end
